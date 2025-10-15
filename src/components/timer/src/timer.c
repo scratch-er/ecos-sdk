8d37879568e25b29ca46c9ddaae6a10222051a47
xhtimmo <slwhx2295317870@outlook.com>
2025-10-15 18:07:11 +0800

    fix: delay

--- src/components/timer/src/timer.c
+++ src/components/timer/src/timer.c
@@ -1,12 +1,12 @@
 #include "timer.h"
+#include "stdio.h"
 #include "generated/autoconf.h"
 
 void delay_ms(uint32_t val){
-    // 1ms = 50MHz /
     REG_TIM0_CONFIG = (uint32_t)0x0000;
-    REG_TIM0_DATA = (uint32_t)(CONFIG_CPU_FREQ_MHZ * 1000 - 1);
-    for (int i = 1; i <= val; ++i)
+    for (uint32_t i = 1; i <= val; ++i)
     {
+        REG_TIM0_DATA = (uint32_t)(CONFIG_CPU_FREQ_MHZ * 1000 - 1);
         REG_TIM0_CONFIG = (uint32_t)0x0001; // irq disable, count down, continuous mode, timer enable
         while (REG_TIM0_DATA == 0)
             ;
