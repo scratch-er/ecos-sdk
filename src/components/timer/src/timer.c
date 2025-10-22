6f7acaa1986e0eced8e2aed3735127bfc32c1b31
xhtimmo <slwhx2295317870@outlook.com>
2025-10-22 15:33:37 +0800

    fix: delay_ms

--- src/components/timer/src/timer.c
+++ src/components/timer/src/timer.c
@@ -11,15 +11,9 @@ void delay_us(uint32_t val){
 }
 
 void delay_ms(uint32_t val){
-    REG_TIM0_CONFIG = (uint32_t)0x0000;
-    for (uint32_t i = 1; i <= val; ++i)
-    {
-        REG_TIM0_DATA = (uint32_t)(CONFIG_CPU_FREQ_MHZ * 1000 - 1);
-        REG_TIM0_CONFIG = (uint32_t)0x0001; // irq disable, count down, continuous mode, timer enable
-        while (REG_TIM0_DATA == 0)
-            ;
-        REG_TIM0_CONFIG = (uint32_t)0x0000;
-    }
+    delay_us(val * 1000);
+}
+
 void delay_s(uint32_t val){
     delay_ms(val * 1000);
 }
