d4fb059f843679ad01902c90afe3e0941b5caf68
XHTimmo <xhtimmo@outlook.com>
2025-11-25 16:03:42 +0800

    feat: gpio support 64 bits

--- components/gpio/src/gpio.c
+++ components/gpio/src/gpio.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include "gpio.h"
 #include "board.h"
+#include "generated/autoconf.h"
 
 static void gpio_set_output(uint32_t io_num){
 #ifdef CONFIG_GPIO_IP_ID == 0
@@ -24,10 +25,10 @@ void gpio_config(const gpio_config_t* config)
         return;
     }
     
-    uint32_t gpio_pin_mask = config->pin_bit_mask;
+    uint64_t gpio_pin_mask = config->pin_bit_mask;
     uint32_t io_num = 0;
     
-    while (io_num < 16){
+    while (io_num < 64){
         if ((gpio_pin_mask >> io_num) & 0x01){
             if (config->mode == GPIO_MODE_INPUT){
                 gpio_set_input(io_num);
